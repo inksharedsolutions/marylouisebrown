a613f3811b0886ad5e17dd250a87a8a9a001fdd2
inksharedsolutions <webdev.inkshared@gmail.com>
2020-10-12 16:33:09 +0800

    changes illustrations 10/11/2020

--- src/components/artwork.js
+++ src/components/artwork.js
@@ -7,24 +7,24 @@ import Img3 from '../../static/illustration/3.jpg'
 import Img4 from '../../static/illustration/4.jpg'
 import Img5 from '../../static/illustration/5.jpg'
 import Img6 from '../../static/illustration/6.jpg'
-import Img7 from '../../static/illustration/7.jpg';
-import Img8 from '../../static/illustration/8.jpg';
-import Img9 from '../../static/illustration/9.jpg';
-import Img10 from '../../static/illustration/10.jpg';
-import Img11 from '../../static/illustration/11.jpg';
-import Img12 from '../../static/illustration/12.jpg';
-import Img13 from '../../static/illustration/13.jpg';
-import Img14 from '../../static/illustration/14.jpg';
-import Img15 from '../../static/illustration/15.jpg';
-import Img16 from '../../static/illustration/16.jpg';
-import Img17 from '../../static/illustration/17.jpg';
-import Img18 from '../../static/illustration/18.jpg';
-import Img19 from '../../static/illustration/19.jpg';
-import Img20 from '../../static/illustration/20.jpg';
+// import Img7 from '../../static/illustration/7.jpg';
+// import Img8 from '../../static/illustration/8.jpg';
+// import Img9 from '../../static/illustration/9.jpg';
+// import Img10 from '../../static/illustration/10.jpg';
+// import Img11 from '../../static/illustration/11.jpg';
+// import Img12 from '../../static/illustration/12.jpg';
+// import Img13 from '../../static/illustration/13.jpg';
+// import Img14 from '../../static/illustration/14.jpg';
+// import Img15 from '../../static/illustration/15.jpg';
+// import Img16 from '../../static/illustration/16.jpg';
+// import Img17 from '../../static/illustration/17.jpg';
+// import Img18 from '../../static/illustration/18.jpg';
+// import Img19 from '../../static/illustration/19.jpg';
+// import Img20 from '../../static/illustration/20.jpg';
 
 const Artwork = () =>{
     
-    const art = [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10, Img11, Img12, Img13, Img14, Img15, Img16, Img17, Img18, Img19, Img20];
+    // const art = [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10, Img11, Img12, Img13, Img14, Img15, Img16, Img17, Img18, Img19, Img20];
 
     return (
         <>
@@ -59,37 +59,6 @@ const Artwork = () =>{
                         <img 
                         alt={`artwork-img`} 
                         src={Img5} />
-                    </div>
-                </div>
-                <div className="art-row">
-                
-                    <div className="art-columns">
-                        <img 
-                        alt={`artwork-img`} 
-                        src={Img7} />
-                    </div>
-
-                    <div className="art-columns">
-                        <div className="two-columns">
-                            <img 
-                            alt={`artwork-img`} 
-                            src={Img8} />
-                            <img 
-                            alt={`artwork-img`} 
-                            src={Img9} />
-                        </div>                
-                        <img 
-                        alt={`artwork-img`} 
-                        src={Img10} />
-                    </div>
-
-                    <div className="art-columns">
-                        <img 
-                        alt={`artwork-img`} 
-                        src={Img11} />
-                        <img 
-                        alt={`artwork-img`} 
-                        src={Img12} />
                     </div>
                 </div>
             </section>        

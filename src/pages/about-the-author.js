c27a46b6abb8f8f2c54ab242bb6cc389d030df80
Murielle Beronga <mberonga.stratton@gmail.com>
2020-11-09 16:24:55 +0800

    author requests 11062020

--- src/pages/about-the-author.js
+++ src/pages/about-the-author.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import Nav from '../components/nav'
 import Banner from '../components/non-front-banner'
-import AuthorImg from '../../static/author/dummy.png'
+import AuthorImg from '../../static/author/author.jpg'
 
 const ATB = (props)=>{
 	
